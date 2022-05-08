4347d3ef2e53d28698cc70180a86f6d0e9e84847
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-05-08 11:42:35 -0300

    agregando productos y link a desciption

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 import {
   CardGroup,
   Card,
@@ -16,6 +17,19 @@ import {
 } from 'reactstrap';
 
 function Products() {
+
+  const url = 'http://localhost:3000/api/products'
+  const [todos, setTodos] = useState()
+  const fetchApi = async () => {
+    const response = await fetch(url);
+    const responseJson = await response.json()
+    setTodos(responseJson.products)
+  }
+
+  useEffect(() => {
+    fetchApi();
+  }, [])
+
   const [dropdown, setDropdown] = useState(false);
 
   const abrirCerrarDropdown = () => {
@@ -44,81 +58,44 @@ function Products() {
       </Dropdown>
 
       <CardGroup>
-        <Card>
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/318/180"
-            top
-            width="100%"
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              Card title
-            </CardTitle>
-            <CardSubtitle
-              className="mb-2 text-muted"
-              tag="h6"
-            >
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-            </CardText>
-            <Button>
-              Button
-            </Button>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/318/180"
-            top
-            width="100%"
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              Card title
-            </CardTitle>
-            <CardSubtitle
-              className="mb-2 text-muted"
-              tag="h6"
-            >
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              This card has supporting text below as a natural lead-in to additional content.
-            </CardText>
-            <Button>
-              Button
-            </Button>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/318/180"
-            top
-            width="100%"
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              Card title
-            </CardTitle>
-            <CardSubtitle
-              className="mb-2 text-muted"
-              tag="h6"
-            >
-              Card subtitle
-            </CardSubtitle>
-            <CardText>
-              This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
-            </CardText>
-            <Button>
-              Button
-            </Button>
-          </CardBody>
-        </Card>
+
+        {!todos ? 'cargando' : todos.map((todo, index) => {
+          return (
+            <>
+              <Card>
+                <CardImg
+                  alt="Card image cap"
+                  src="https://picsum.photos/318/180"
+                  top
+                  width="100%"
+                />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    <td key={index}>{todo.name}</td>
+                  </CardTitle>
+                  <CardSubtitle
+                    className="mb-2 text-muted"
+                    tag="h6"
+                  >
+                    <td key={index}>{todo.price}</td>
+                  </CardSubtitle>
+                  <CardText>
+                    <td key={index}>{todo.description}</td>
+                  </CardText>
+                  <Link to={`/${todo.id}`}>
+                    <Button>
+                      Description
+                    </Button>
+                  </Link>
+                </CardBody>
+              </Card>
+            </>
+          )
+        })}
+
+
+
+
       </CardGroup>
 
     </>
